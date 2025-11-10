058cc26c9a31fe7c717fc12536a95f6c05eb4189
AlexE.Dev <locha8472@t-online.de>
2025-11-10 12:40:02 +0100

    code update

---
+++ script.js
@@ -0,0 +1,14 @@
+let images [
+"ai-generated-7790616_1280.jpg",
+"aliens-7774364_1280.jpg",
+"astronaut-5906015_1280.jpg",
+"astronomy-3105876_1280.jpg",
+"earth-11015_1280.jpg",
+"earth-7377061_1280.jpg",
+"neptune-5550216_1280.jpg",
+"planet-11613_1280.jpg",
+"saturn-341379_1280.jpg",
+"spaceship-5570682_1280.jpg",
+"spaceship-7839420_1280.jpg",
+"star-trek-8658148_1280.jpg"
+];
