4d0a02e6568122e4f2be2d23456e4f83883d2714
AlexE.Dev <locha8472@t-online.de>
2025-11-11 16:44:43 +0100

    new edit dialog in html,css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let images [
+const images = [
 "ai-generated-7790616_1280.jpg",
 "aliens-7774364_1280.jpg",
 "astronaut-5906015_1280.jpg",
@@ -12,3 +12,36 @@ let images [
 "spaceship-7839420_1280.jpg",
 "star-trek-8658148_1280.jpg"
 ];
+
+
+function init() {
+    let NewImages = document.getElementById('import-image');
+    NewImages.innerHTML = ' ';
+    
+// problem das erste bold wird nicht angezeigt
+    for (let index = 0; index < images.length; index++) {
+       NewImages.innerHTML += `<img src="./img/${images[index]}" onclick="dialogOpen()" alt="Fotogram">`;
+    }
+    
+
+}
+
+
+
+    // Zugriff auf das Dialogelement mit seiner ID
+    
+    const dialogRef = document.getElementById('myDialog');
+    // Funktion zum Öffnen des Dialogs
+    function dialogOpen() {
+        
+        dialogRef.showModal();           // Öffnet das Dialogfenster modal (blockiert Hintergrund)
+        dialogRef.classList.add('opened'); // Fügt die CSS-Klasse hinzu → löst Animation & Backdrop aus
+    }
+
+    // Funktion zum Schließen des Dialogs
+    function dialogClose() {
+        // const dialogRef = document.getElementById('myDialog');
+        dialogRef.close();                // Schließt den Dialog
+        dialogRef.classList.remove('opened'); // Entfernt die CSS-Klasse → Animation zurücksetzen
+    }
+
